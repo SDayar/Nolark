70fe29573ed22e7612e5647d3aa1f0e513e4105f
Saîfidine <127401520+SDayar@users.noreply.github.com>
2023-04-06 09:57:00 +0200

    Update header.php

--- php/header.php
+++ php/header.php
@@ -1,7 +1,7 @@
 <?php     $scriptName = filter_input(INPUT_SERVER, "SCRIPT_NAME");
           $pageActuelle = substr($scriptName, strrpos($scriptName,"/") + 1);
           if($pageActuelle == "index.php"){
-           $dirIndex = "public_html/";
+           $dirIndex = " ";
            $dirPages = "../pages/";
            } else{
               $dirIndex ="../public_html/";
